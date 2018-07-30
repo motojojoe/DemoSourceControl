93ba0d88ee21d738ac2ef7468c4f976fd06f3a9d
Atthachai Meethong <atthachai.mee@gmail.com>
2018-07-30 21:29:07 +0700

    Set version label by view controller

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        versionLabel.text = "Version 3"
     }
 
     override func didReceiveMemoryWarning() {
